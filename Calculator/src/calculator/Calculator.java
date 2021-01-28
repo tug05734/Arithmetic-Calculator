ff6ddb197fce0b476a8cd45588a603815aa2bb00
Rajat Rajan <tug05734@temple.edu>
2021-01-27 19:55:06 -0500

    Added switch case and user input

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -5,6 +5,8 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rajat
@@ -14,8 +16,54 @@ public class Calculator {
     /**
      * @param args the command line arguments
      */
+    static boolean stillRunning = true;
+    static int number1, number2, operation;
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        while(stillRunning){
+            System.out.print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nPlease input the number of the operation you would like to do: ");
+            operation = in.nextInt();
+            System.out.print("\nInput first number: ");
+            number1 = in.nextInt();
+            System.out.print("\nInput second number: ");
+            number2 = in.nextInt();
+            
+            switch (operation){
+                case 1:
+                    add(number1, number2);
+                    break;
+                case 2: 
+                    subtract(number1, number2);
+                    break;
+                case 3:
+                    multiply(number1, number2);
+                    break;
+                case 4:
+                    divide(number1, number2);
+                    break;
+                default:
+                    System.out.println("\nIncorrect operation input");
+                    stillRunning = false;
+            }
+        }    
     }
+    
+    private static int add(int x, int y){
+        return  x + y;
+    }
+    
+    final static int subtract(int x, int y){
+        return x - y;
+    }
+    
+    private static int multiply(int x , int y){
+        return x * y;
+    }
+    
+    static double divide(int x, int y){
+        return x / y;
+    }
+    
     
 }
